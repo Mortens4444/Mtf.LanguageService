f3e0996715f5897db91d3c28477525527d25a2d6
Mortens <mortens.4444@gmail.com>
2025-11-19 22:27:16 +0100

    Mtf.LanguageService.MAUI NuGet package release 1.0.48

---
+++ Mtf.LanguageService.MAUI/Converters/EnumDescriptionTranslationConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+
+namespace Mtf.LanguageService.MAUI.Converters;
+
+internal class EnumDescriptionTranslationConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return String.Empty;
+        }
+
+        var type = value.GetType();
+        var name = value.ToString() ?? String.Empty;
+
+        if (!type.IsEnum)
+        {
+            return Lng.Elem(name);
+        }
+
+        var member = type.GetMember(name).FirstOrDefault();
+        if (member == null)
+        {
+            return Lng.Elem(name);
+        }
+
+        var descAttr = member.GetCustomAttribute<DescriptionAttribute>();
+        var description = descAttr?.Description ?? name;
+
+        return Lng.Elem(description);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => value;
+}
---
+++ Mtf.LanguageService.MAUI/Converters/TranslationConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Mtf.LanguageService.MAUI.Converters;
+
+public class TranslationConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return String.Empty;
+        }
+
+        var key = value is string s ? s : value.ToString();
+        if (String.IsNullOrEmpty(key))
+        {
+            return String.Empty;
+        }
+
+        try
+        {
+            return Lng.Elem(key);
+        }
+        catch
+        {
+            return key;
+        }
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
+}
